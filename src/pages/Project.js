7df8e49cd033830eac281f3f8ffd9e8b48005138
FoJkee <romanovsky0815@gmail.com>
2023-04-29 21:43:12 +0300

    fix: button dark-sun

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,19 +1,33 @@
 import img from './../img/projects/02-big.jpg';
 import BtnGitHub from '../components/btnGitHub/BtnGitHub';
+import { useParams } from 'react-router-dom';
+import { projects } from './../helpers/projectList';
 
 const Projeсt = () => {
+    const { id } = useParams();
+    const project = projects[id];
+
+    console.log(project);
+
     return (
         <main className='section'>
             <div className='container'>
                 <div className='project-details'>
-                    <h1 className='title-1'>Video service</h1>
+                    <h1 className='title-1'>{project.title}</h1>
 
-                    <img src={img} alt='' className='project-details__cover' />
+                    <img
+                        src={project.imgBig}
+                        alt={project.title}
+                        className='project-details__cover'
+                    />
 
                     <div className='project-details__desc'>
-                        <p>Skills: React, Node.js, MongoDB</p>
+                        <p>Skills: {project.skills}</p>
                     </div>
-                    <BtnGitHub link='https://github.com' />
+
+                    {project.gitHubLink && (
+                        <BtnGitHub link='https://github.com' />
+                    )}
                 </div>
             </div>
         </main>
